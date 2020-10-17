712aa73090e16e9024f31f353efe99b318c8f79d
Dietmar Ruef <dietmar.ruef@fenergo.com>
2020-10-17 17:32:45 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const puppeteer = require('puppeteer-extra')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+puppeteer.use(AdblockerPlugin({
+    blockTrackers: true
+}))
+
+var jsonParser = bodyParser.json()
+
+app.post('/download', jsonParser, async (req, res) => {
+    const requestBody = req.body;
+
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto(requestBody.Url);
+    const content = await page.content();
+    await browser.close();    
+
+    res.status(200)
+        .send({
+            "Content": content            
+        });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
